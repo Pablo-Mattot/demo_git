140d30d5c118b3275cd47151d59cdfc8b39311fb
Pablo <pablo.mattot@student.ehb.be>
2020-08-29 15:15:11 +0200

    created method to calculate a sum

--- src/main/java/be/ehb/demo_git/Controller/CalcController.java
+++ src/main/java/be/ehb/demo_git/Controller/CalcController.java
@@ -1,10 +1,19 @@
 package be.ehb.demo_git.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class CalcController {
 
-
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    @ResponseBody
+    public Integer som(@RequestParam(value = "x") Integer x,
+                       @RequestParam(value = "y") Integer y){
+        return x+y;
+    }
 
 }
