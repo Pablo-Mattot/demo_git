c989ede4d3a91b04d601bf9875d27ffc6ecb4b50
Pablo <pablo.mattot@student.ehb.be>
2020-08-29 15:00:04 +0200

    initial commit

---
+++ src/test/java/be/ehb/demo_git/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.demo_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
